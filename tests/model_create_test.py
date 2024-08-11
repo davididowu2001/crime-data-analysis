2ae2419750a7727161de137dfef78307a855a969
davididowu2001 <idowu.subomi9@gmail.com>
2024-08-11 21:46:44 +0100

    adding testcase

---
+++ tests/model_create_test.py
@@ -0,0 +1,17 @@
+'''
+this file is used to test the model creation'''
+import pytest
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+from Model_exploration.model_creation import load_data, clean_data, split_data, main
+
+
+def test_load_data():    
+    data = load_data()
+    assert isinstance(data, pd.DataFrame)
+    assert data.shape[0] > 0
+    assert data.shape[1] > 0
+
+
